97de62a9b054729398eb507e784b9a32429065f7
Me <me@mes-macbook-pro.local>
2018-01-26 00:01:19 +0100

    test

--- U11/src/matrix/TestStudent.java
+++ U11/src/matrix/TestStudent.java
@@ -1,16 +1,17 @@
 package matrix;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class TestStudent {
 
 	Matrix matrix1= new Matrix(2, 2, new FloatArithmetic());
 	Matrix matrix2= new Matrix(2, 2, new FloatArithmetic());
+	Matrix matrix3= new Matrix(2, 2, new FloatArithmetic());
+
 	
-	matrix1.setCell(0, 0,1 );
-	matrix1.setCell(0, 1,2 );
-	matrix1.setCell(1, 0,3 );
-	matrix1.setCell(1, 1,4 );
+	
 	
 	
 	
@@ -18,25 +19,71 @@ public class TestStudent {
 	public void MatrixTest(){
 		
 		
+		matrix1.setCell(0, 0,1 );
+		matrix1.setCell(0, 1,2 );
+		matrix1.setCell(1, 0,3 );
+		matrix1.setCell(1, 1,4 );
+		
+		
+		
 		
 	}
 	public void getCellTest() {
 		
+		matrix1.setCell(0, 0,1 );
+		matrix1.setCell(0, 1,2 );
+		matrix1.setCell(1, 0,3 );
+		matrix1.setCell(1, 1,4 );
+		assertEquals(1, matrix1.getCell(0,0));
+		assertEquals(4, matrix1.getCell(1,1));
 		
 		
 	}
     public void setCellTest() {
     	
+    	matrix1.setCell(1, 1,4 );
+    	
+    	assertEquals( 4, matrix1.getCell(1,1));
 		
 	}
 	
     public void addTest() {
+    	    matrix1.setCell(0, 0,1 );
+		matrix1.setCell(0, 1,2 );
+		matrix1.setCell(1, 0,3 );
+		matrix1.setCell(1, 1,4 );
+		
+		matrix2.setCell(0, 0,4 );
+		matrix2.setCell(0, 1,3 );
+		matrix2.setCell(1, 0,1 );
+		matrix2.setCell(1, 1,0 );
+		
+		matrix3.setCell(0, 0, 5);
+		matrix3.setCell(0, 1, 5);
+		matrix3.setCell(1, 0, 4);
+		matrix3.setCell(1, 1, 4);
+
+		assertEquals(matrix3, matrix1.add(matrix2));
+    	
     	
 		
 	}
     public void mulTest() {
     	
-		
+    	    matrix1.setCell(0, 0,1 );
+ 		matrix1.setCell(0, 1,2 );
+ 		matrix1.setCell(1, 0,3 );
+ 		matrix1.setCell(1, 1,4 );
+ 		
+ 		matrix2.setCell(0, 0,4 );
+ 		matrix2.setCell(0, 1,3 );
+ 		matrix2.setCell(1, 0,1 );
+ 		matrix2.setCell(1, 1,0 );
+ 		
+ 		matrix3.setCell(0, 0, 5);
+ 		matrix3.setCell(0, 1, 5);
+ 		matrix3.setCell(1, 0, 4);
+ 		matrix3.setCell(1, 1, 4);
 	}
     public void transposeTest() {
     	
