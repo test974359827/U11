a29298031817c3e30b5e2ee3106ba0778d7e8c67
Shayan <shayandavarifard@yahoo.com>
2018-01-26 00:04:55 +0100

    s

--- U11/src/matrix/TestStudent.java
+++ U11/src/matrix/TestStudent.java
@@ -7,16 +7,12 @@ public class TestStudent {
 	Matrix matrix1= new Matrix(2, 2, new FloatArithmetic());
 	Matrix matrix2= new Matrix(2, 2, new FloatArithmetic());
 	
-	matrix1.setCell(0, 0,1 );
-	matrix1.setCell(0, 1,2 );
-	matrix1.setCell(1, 0,3 );
-	matrix1.setCell(1, 1,4 );
+	
 	
 	
 	
 	@Test
 	public void MatrixTest(){
-		
 		
 		
 	}
