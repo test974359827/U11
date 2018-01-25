4f7fc75486df65418bd50922bc863fff1b971174
Me <me@mes-macbook-pro.local>
2018-01-25 22:53:26 +0100

    Test

--- U11/src/matrix/TestStudent.java
+++ U11/src/matrix/TestStudent.java
@@ -4,11 +4,52 @@ import org.junit.Test;
 
 public class TestStudent {
 
+	Matrix matrix1= new Matrix(2, 2, new FloatArithmetic());
+	Matrix matrix2= new Matrix(2, 2, new FloatArithmetic());
+	
+	matrix1.setCell(0, 0,1 );
+	matrix1.setCell(0, 1,2 );
+	matrix1.setCell(1, 0,3 );
+	matrix1.setCell(1, 1,4 );
+	
 	
 	@Test
-	public void haha(){
+	public void MatrixTest(){
+		
 		
 		
 	}
+	public void getCellTest() {
+		
+		
+		
+	}
+    public void setCellTest() {
+    	
+		
+	}
+	
+    public void addTest() {
+    	
+		
+	}
+    public void mulTest() {
+    	
+		
+	}
+    public void transposeTest() {
+    	
+    	
+    }
+    public void getMinMaxTest() {
+    	
+    	
+    }
+    
+    public void resizeTest() {
+    	
+    	
+    }
+	
 }
 	
