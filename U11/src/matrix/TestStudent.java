b0b993abdbe1f819b9967aceabbb229ad43fc16b
Me <me@mes-macbook-pro.local>
2018-01-26 01:22:00 +0100

    Tests

--- U11/src/matrix/TestStudent.java
+++ U11/src/matrix/TestStudent.java
@@ -1,4 +1,11 @@
 package matrix;
+/**
+ * @author       Shayan     Davari fard
+ * @author   Mohammadrahim     Masoumi
+ * @author        Arian      Tashakkornojehdehi
+ * 
+ * @version 1
+ */
 
 import static org.junit.Assert.assertEquals;
 
@@ -10,6 +17,11 @@ public class TestStudent {
 	Matrix<Integer> matrix1= new Matrix<Integer>(2, 2, new IntegerArithmetic());
 	Matrix<Integer> matrix2= new Matrix<Integer>(2, 2, new IntegerArithmetic());
 	Matrix<Integer> matrix3= new Matrix<Integer>(2, 2, new IntegerArithmetic());
+	Matrix<Integer> matrix4= new Matrix<Integer>(2, 3, new IntegerArithmetic());
+	Matrix<Integer> matrix5= new Matrix<Integer>(3, 2, new IntegerArithmetic());
+	Matrix<Integer> matrix6= new Matrix<Integer>(3, 1, new IntegerArithmetic());
+
+
 
 	
 	
@@ -80,7 +92,7 @@ public class TestStudent {
 	@Test
     public void mulTest() {
     	
-    	matrix1.setCell(0, 0,1 );
+     	matrix1.setCell(0, 0,1 );
  		matrix1.setCell(0, 1,2 );
  		matrix1.setCell(1, 0,3 );
  		matrix1.setCell(1, 1,4 );
@@ -90,29 +102,70 @@ public class TestStudent {
  		matrix2.setCell(1, 0,1 );
  		matrix2.setCell(1, 1,0 );
  		
- 		matrix3.setCell(0, 0, 5);
- 		matrix3.setCell(0, 1, 5);
- 		matrix3.setCell(1, 0, 4);
- 		matrix3.setCell(1, 1, 4);
+ 		matrix3.setCell(0, 0, 6);
+ 		matrix3.setCell(0, 1, 3);
+ 		matrix3.setCell(1, 0, 16);
+ 		matrix3.setCell(1, 1, 9);
+ 		
+		assertEquals(matrix3, matrix1.mul(matrix2));
+
 	}
 	
 	@Test
     public void transposeTest() {
+		matrix4.setCell(0, 0,1 );
+ 		matrix4.setCell(0, 1,2 );
+ 		matrix4.setCell(0, 2,2 );
+ 		matrix4.setCell(1, 0,3 );
+ 		matrix4.setCell(1, 1,4 );
+ 		matrix4.setCell(1, 1,5 );
+ 		
+		matrix5.setCell(0, 0,1 );
+ 		matrix5.setCell(0, 1,3 );
+ 		matrix5.setCell(1, 0,2 );
+ 		matrix5.setCell(1, 1,4 );
+ 		matrix5.setCell(2, 0,2 );
+ 		matrix5.setCell(2, 1,5 );
+ 		
+ 		
+		assertEquals(matrix5, matrix4.transpose());
+
     	
     	
     }
 	
 	@Test
     public void getMinMaxTest() {
-    	
+		
+		    matrix1.setCell(0, 0,1 );
+			matrix1.setCell(0, 1,2 );
+			matrix1.setCell(1, 0,3 );
+			matrix1.setCell(1, 1,4 );
+		
+			assertEquals(new Integer(4), matrix1.getMinMax(false));
+			assertEquals(new Integer(1), matrix1.getMinMax(true));
+
+			
+
     	
     }
     
 	
 	@Test
     public void resizeTest() {
+		
+		    matrix1.setCell(0, 0,1 );
+			matrix1.setCell(0, 1,2 );
+			matrix1.setCell(1, 0,3 );
+			matrix1.setCell(1, 1,4 );
+			
+		//	matrix6= matrix1.resize(1, -1);
+			matrix6.setCell(0, 0,1);
+			matrix1.setCell(1, 0,3 );
+			matrix1.setCell(2, 0,0 );
     	
-    	
+			assertEquals(matrix6, matrix1.resize(1, -1));
+
     }
 	
 }
